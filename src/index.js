b63f3c20300da1149757370ec4c2b855ee05e904
another-dave <whatsyourfavouritehummingnoise@gmail.com>
2015-06-07 23:00:19 +0100

    Making debug 'pretty-print' format

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import parser from './parser.js'
 
 var start = parser('/Maze/Location/easy/start')
 start.then(function (value) {
-	console.log(JSON.stringify(value))
+	console.log(JSON.stringify(value, null, '\t'))
 })
